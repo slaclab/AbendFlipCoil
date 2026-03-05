d0e6ebb9da20ad8de2ac67d4b33627e541178228
Lukas Ho-Koziol <lujko@slac.stanford.edu>
2026-03-05 14:55:36 -0800

    The documentation update for the multimeter task part, need to add more pv support

--- CounterPortDriverApp/src/flipcoil_util.cpp
+++ CounterPortDriverApp/src/flipcoil_util.cpp
@@ -5,73 +5,78 @@
 #include<unistd.h>
 #include <sstream>
 
+/**
+ * @brief Function for thread calculating field strength
+ *
+ * This function sends commands to an Hp3458a multimeter,
+ * These commands set the multimeter to a known state and then 
+ * prompt the multimeter to take some amount of readings, as set by 
+ * the user through a PV otherwise a default value is used.
+ * These readings are then used to calculate field strength 
+ * readings which are stored into separate EPICS PVs for retreival
+ * by the user.
+ **/
 void FlipCoilDriver::multimeterTask(void)
 {
-  
-  
-  //blmeasbl measurement
+  // Block of commands for setting the multimeter to a known state 
+  asynPrint(pasynUserPort, ASYN_TRACE_FLOW, "Beginning the multimeter task\n");
+  pasynOctetSyncIO->flush(pasynUserPort); //Purge command buffer of incomplete data 
+  _writeRead("PRESET NORM\r\n"); //Set multimeter to known settings defined in more detail in manual 
+  _writeRead("TRIG HOLD\r\n"); //Tells multimeter to wait for trigger signal
+  _writeRead("MEM FIFO\r\n"); //Turns memory on in the multimeter, stores samples first in first out 
+  _writeRead("NPLC 10\r\n"); //Add some noise filtering 
+  _writeRead("NRDGS 170, TIMER\r\n"); //Tells the multimeter it take an amount of readings with a gap indicated by the timer between each reading 
+  _writeRead("TIMER 1\r\n"); //Tells the multimeter how long to wait between each reading 
 
-  pasynOctetSyncIO->flush(pasynUserPort);
-  printf("\n\nMultimeter task");
-  /**
-   * Setup for the multimeter section
-  **/ 
-  _writeRead("PRESET NORM\r\n");
-  _writeRead("MSIZE?\r\n");
+  //Assorted variable declarations 
+  float prev = 0; //Storage for the previous variable 
+  vector<float> pos_samples; //Vector for positive samples of the waveform
+  vector<float> neg_samples; //Vector for negative samples of the waveform 
+  int crossings = 0; //Tracker for seen zero crossings 
+  size_t nBytesOut, nBytesIn; //Variables for pasynOctet calls 
+  int eomReason; // Another variable for pasynOctet calls, call will put if terminal character encountered in this variable 
+ 
 
-  _writeRead("END ALWAYS\r\n");
-  _writeRead("TRIG HOLD\r\n");
-  _writeRead("MEM FIFO\r\n");
-  _writeRead("NPLC 10\r\n");
-  _writeRead("NRDGS 170, TIMER\r\n");
-  _writeRead("TIMER 1\r\n");
-
-  float variable;
-  vector<float> coil_samples;
-  float prev = 0;
-  int x = 0;
-
-  //As soon as we break this loop we're finding the bits we're trying to integrate 
-  vector<float> pos_samples;
-  vector<float> neg_samples;
-  int crossings = 0;
-  int test;
-  size_t nBytesOut, nBytesIn;
-  int eomReason;
+  //While loop where we take readings and isolate one period of the waveform we're analyzing 
   while (crossings < 3)
   {
-    _writeRead("TRIG SGL\r\n");
-    sleep(172);
-    pasynOctetSyncIO->flush(pasynUserPort);
-    pasynOctetSyncIO->write(pasynUserPort, "RMEM 1,170;\r\n", 11, 5.0, &nBytesOut);
+    _writeRead("TRIG SGL\r\n"); //Command triggers the multimeter to begin readings 
+    sleep(172); //TODO: define this based on user provided Timer time 
+    pasynOctetSyncIO->write(pasynUserPort, "RMEM 1,170;\r\n", 11, 5.0, &nBytesOut); //Command that tells the multimeter to send it's readings over gpib to us 
     
-
-    //printf("THe bytes we received: %d", nBytesIn);
+    //For loop that retrieves readings from the multimeter TODO: Define this on user provided num samples.
     for (int i = 0; i < 170; i++)
     {
+      //Read command, if there's an error well shoot
       asynStatus read_status = pasynOctetSyncIO->read(pasynUserPort, cmdBuffer, 8192, 5.0, &nBytesIn, &eomReason);
       if (read_status != asynSuccess)
       {
-        printf("Multimeter read errored for some reason please retry\n");
+        asynPrint(pasynUserPort, ASYN_TRACE_ERROR, "Unable to read from multimeter, please restart thread and multimeter, error message: %s\n", pasynUser->errorMessage);
         return;
       }
+
+      //process the readings, strip the comma, convert to a float 
       char * token = strtok(cmdBuffer, ",");
       float reading = stof(token);
-      printf("Reading: %f, Reading No. %d, Crossing #%d\n", reading, i, crossings);
+      asynPrint(pasynUserPort, ASYN_TRACE_FLOW, "Reading: %f, reading no: %d, crossing: %d\n", reading, i, crossing);
+      
+      //Check for a zero crossing, basically pos * neg = neg so anytime current reading * prev reading < 0 we've got a zero crossing 
       if (reading * prev < 0)
       {
         crossings += 1;
       }
       if (crossings == 3)
       {
-        printf("\nAcquired complete sine wave");
+        asynPrint(pasynUserPort, ASYN_TRACE_FLOW, "Read complete waveform, leaving while loop\n");
         break;
       }
+
+      //Check we've found at least one zero crossing before storing data, if we haven't this reading's cycle will be incomplete.
       if (crossings > 0)
       {
         if (reading < 0)
         {
-          neg_samples.push_back(-1 * reading);
+          neg_samples.push_back(-1 * reading); // we multiply by -1 to make values positive so that the integral calc works properly 
         }
         else 
         {
@@ -80,14 +85,9 @@ void FlipCoilDriver::multimeterTask(void)
       }
       prev = reading;
     }
-    pasynOctetSyncIO->write(pasynUserPort, "MEM FIFO\r\n", 8, 5.0, &nBytesOut);
+    // TODO: Figure out if this is necessary,best practice would be get enough samples that we don't need another batch
+    pasynOctetSyncIO->write(pasynUserPort, "MEM FIFO\r\n", 8, 5.0, &nBytesOut); //In case we need another set of samples, turn the memory back on/
 
-  }
-  //TODO Remove the return, start doing the integration again
-  //return;
-  for (float x: pos_samples)
-  {
-    printf("\nSanity checking pos samples vector: %f", x);
   }
   //Then the time integral is calculated within coil_samples coilintpeak
   float pos_peak = coilIntPeak(COIL_DELTA, pos_samples);
@@ -95,16 +95,8 @@ void FlipCoilDriver::multimeterTask(void)
   //Third a a time integral of the negative samples is calculated coilintpeak
   float neg_peak = -1 * coilIntPeak(COIL_DELTA, neg_samples);
   
-  //Check for forward and reverse half cycles, takes results from coilintpeak 
-  //TODO Figure out why this if statement is necessary
+  //In theory we'll want to track over time or do multiple measurements, store measurements from measured cycle for further use.
   printf("Calculated Pos Peak: %f Calculated Neg Peak: %f\n\n", pos_peak, neg_peak);
-  /**
-  if (neg_peak * pos_peak >= 0)
-  {
-   printf("Problems, didn't receive coil voltages most likely");
-    throw runtime_error("Neg_peak * pos_peak returned a positive number or was zero, should be impossible");
-  }
-  **/
   if (pos_peak > 0 )
   {
     vt_pos_mult.push_back(pos_peak);
@@ -116,7 +108,7 @@ void FlipCoilDriver::multimeterTask(void)
     vt_pos_mult.push_back(neg_peak);
   }
   vt_avg_mult.push_back((vt_pos_mult.back() - vt_neg_mult.back()) / 2);
-  //printf("Integrated calculation, %g\n", vt_avg[i]);
+
   //Finding the mean of integrated voltage values
   float sum = 0;
   for(float x : vt_avg_mult)
@@ -128,6 +120,7 @@ void FlipCoilDriver::multimeterTask(void)
   float avg = sum / vt_avg_mult.size();
   Avg_Int_Mult.store(avg);
   printf("\n\n\nAveraged integral %g", avg);
+
   //Finding standard deviation
   sum = 0;
   for(float x : vt_avg_mult)
